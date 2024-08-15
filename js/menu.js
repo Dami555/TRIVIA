d8050b8cc81fba7e630c646bfaf08d35aba297c3
Dami555 <castillodamaris2007@gmail.com>
2024-08-15 18:44:28 -0500

    primer comentario

---
+++ js/menu.js
@@ -0,0 +1,7 @@
+function agregar_nombre() {
+    var anterior = document.getElementById("saludo");
+    var actual = localStorage.getItem("name");
+    anterior.innerHTML = anterior.innerHTML + actual;
+  }
+  agregar_nombre(); 
+  
---
+++ js/script.js
@@ -0,0 +1,5 @@
+function usuario() {
+    var nuevo = document.getElementById("usuario").value;
+    localStorage.setItem("name", nuevo);
+    window.location.href = '/TRIVIA/seccion.html';
+}
